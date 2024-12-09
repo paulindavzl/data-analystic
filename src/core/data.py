b60c1f17d540766da73bbdb0359f7070e5aeb172
paulindavzl <paulochz01@gmail.com>
2024-12-09 12:05:34 -0300

    tests for data.py 100%

--- src/core/data.py
+++ src/core/data.py
@@ -10,27 +10,37 @@ class Data:
     def __init__(self):
         self.__method = pd.read_excel
         self.__data = None
-        self.__files = []
 
 
     # abre uma planilha
-    def open(self, filename: str, type: Op[str]='xlsx', max_lines: Op[int]=None):
-        path = FILES + filename
+    def open(self, filename: str, dtype: Op[str]='xlsx', max_lines: Op[int]=None) -> dict:
+        path = FILES + str(filename)
 
-        # garante que a planilha já foi importada
-        if not os.path.exists(path):
-            return {'err': 'data_not_found', 'data': 'spreadsheet'}, path
+        # garante que filename seja str
+        if not isinstance(filename, str):
+            return {'err': 'type_error', 'data': filename, 'expected': 'str'}
+        
+        # garante que type seja str
+        elif not isinstance(dtype, str):
+            return {'err': 'type_error', 'data': dtype, 'expected': 'str'}
+        
+        # garante que max_lines seja int
+        elif max_lines and not isinstance(max_lines, int):
+            return {'err': 'type_error', 'data': max_lines, 'expected': 'int'}
 
         pd.set_option('display.max_rows', max_lines)
-        self.__files.append(filename)
 
         # define o método mais adequado
-        if type == 'xlsx':
+        if dtype == 'xlsx':
             self.__method = pd.read_excel
-        elif type == 'csv':
+        elif dtype == 'csv':
             self.__method = pd.read_csv
         else:
-            return {'err': 'type_not_supported', 'type': type}
+            return {'err': 'type_not_supported', 'type': dtype}
+        
+        # garante que a planilha já foi importada
+        if not os.path.exists(path):
+            return {'err': 'data_not_found', 'data': 'spreadsheet', 'filename': filename}
         
         self.__data = self.__method(path)
 
@@ -38,37 +48,54 @@ class Data:
 
 
     # obtém uma coluna
-    def get_column(self, column_name: str):
-        # garante que a planilha já foi aberta
-        try:
-            if not self.__data:
-                return {'err': 'data_not_defined', 'data': 'spreadsheet'}
-        except:
-            pass
+    def get_column(self, column_name: str, to_list: Op[bool]=False) -> dict:
+        # garante que column_name seja str
+        if not isinstance(column_name, str):
+            return {'err': 'type_error', 'data': column_name, 'expected': 'str'}
         
-        data = self.__data[column_name]
+        # garante que to_list seja bool
+        elif not isinstance(to_list, bool):
+            return {'err': 'type_error', 'data': to_list, 'expected': 'bool'}
+
+        # garante que a planilha já foi aberta
+        elif not isinstance(self.__data, pd.DataFrame):
+            return {'err': 'data_not_defined', 'data': 'spreadsheet', 'expected': 'DataFrame'}
+        
+        data = self.__data[column_name] if not to_list else self.__data[column_name].tolist()
 
         return {'data': data}
     
 
     # obtém várias colunas
-    def get_columns(self, *columns: str):
-         # garante que a planilha já foi aberta
-        try:
-            if not self.__data:
-                return {'err': 'data_not_defined', 'data': 'spreadsheet'}
-        except:
-            pass
+    def get_columns(self, *columns: str, to_list: Op[bool]=False) -> dict:
+        columns = list(columns)
 
-        data = self.__data[list(columns)]
+        # garante que os nomes das colunas sejam str
+        for column in columns:
+            if not isinstance(column, str):
+                return {'err': 'type_error', 'data': column, 'expected': 'str'}
+            
+        # garante que to_list seja bool
+        if not isinstance(to_list, bool):
+            return {'err': 'type_error', 'data': to_list, 'expected': 'bool'}
+
+        # garante que a planilha já foi aberta
+        elif not isinstance(self.__data, pd.DataFrame):
+            return {'err': 'data_not_defined', 'data': 'spreadsheet', 'expected': 'DataFrame'}
+
+        data = self.__data[columns] if not to_list else self.__data[columns].values.tolist()
 
         return {'data': data}
     
 
     # cria uma nova planilha do zero
-    def new(self, data: dict):
+    def new(self, data: dict) -> dict:
+        # garante que data seja dict
+        if not isinstance(data, dict):
+            return {'err': 'type_error', 'data': data, 'expected': 'dict'}
+
         # garante que a planilha não será vazia
-        if len(data) <= 0:
+        elif len(data) <= 0:
             return {'err': 'data_empty',  'data': 'spreadsheet'}
 
         spreadsheet = pd.DataFrame(data)
@@ -77,14 +104,30 @@ class Data:
     
 
     # cria uma pllanilha a partir de outra
-    def save(self, filename: str, data: pd.DataFrame, index: Op[bool]=True, overwrite: Op[bool]=True, test: Op[bool]=False):
-        path = FILES + filename
+    def save(self, filename: str, data: pd.DataFrame, index: Op[bool]=True, overwrite: Op[bool]=True, test: Op[bool]=False) -> dict:
+        path = FILES + str(filename)
 
-        # garante que a planilha não está vazia
-        if data.empty:
-            return {'err': 'data_empty', 'data': 'spreadsheet'}
+        # garante que filename seja str
+        if not isinstance(filename, str):
+            return {'err': 'type_error', 'data': filename, 'expected': 'str'}
         
-        # verifica se já existe uma pllanilha com esse nome
+        # garante que data seja DataFrame
+        elif not isinstance(data, pd.DataFrame) and (not isinstance(data, dict) or not isinstance(data.get('data'), pd.DataFrame)):
+            return {'err': 'type_error', 'data': data, 'expected': 'DataFrame'}
+        
+        # garante que index seja bool
+        elif not isinstance(index, bool):
+            return {'err': 'type_error', 'data': index, 'expected': 'bool'}
+        
+        # garante que overwrite seja bool
+        elif not isinstance(overwrite, bool):
+            return {'err': 'type_error', 'data': overwrite, 'expected': 'bool'}
+        
+        # garante que test seja bool
+        elif not isinstance(test, bool):
+            return {'err': 'type_error', 'data': test, 'expected': 'bool'}
+
+        # verifica se já existe uma planilha com esse nome
         elif self.__file_exists(path, overwrite):
             return {'err': 'spreadsheet_already_exists', 'filename': filename}
         
@@ -92,8 +135,9 @@ class Data:
         elif filename == 'for_tests.xlsx' and not test:
             return {'err': 'invalid_filename', 'filename': filename}
         
-        self.__files.append(filename)
-        
+        # transforma data em DataFrame caso seja dict
+        data = data.get('data') if isinstance(data, dict) else data
+                
         data.to_excel(path, index=index)
 
         return {'result': 'Success'}
@@ -109,16 +153,23 @@ class Data:
 
     
     # apaga um ou mais arquivos específicos
-    def remove(self, *files):
+    def remove(self, *files: str):
         files = list(files)
-        print(files)
         for file in files:
-            if os.path.exists(FILES + file):
-                os.remove(FILES + file)
+            if os.path.exists(FILES + str(file)):
+                os.remove(FILES + str(file))
     
 
     # verifica se um arquivo já existe
-    def __file_exists(self, path: str, overwrite: Op[bool]=False):
+    def __file_exists(self, path: str, overwrite: Op[bool]=False) -> bool:
+        # garante que path seja str
+        if not isinstance(path, str):
+            return {'err': 'type_error', 'data': path, 'expected': 'str'}
+        
+        # garante que overwrite seja str
+        if not isinstance(overwrite, bool):
+            return {'err': 'type_error', 'data': overwrite, 'expected': 'bool'}
+
         return True if os.path.exists(path) and not overwrite else False
 
 
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,18 @@
 import os
 import pytest
+from pandas import DataFrame
 from src.core import FILES
 from src.core.data import Data
 
+
+@pytest.fixture
+def data():
+    return Data()
+
+
 @pytest.fixture()
 def create_test_data():
+    # esta fixture já fica como teste para new(), save(), remove() e eventualmente remove_all()
     filename = 'for_tests.xlsx'
     data = Data()
     df = data.new({
@@ -14,7 +22,7 @@ def create_test_data():
         'years old': [
             0, 0, 0
         ]
-    }).get('data')
+    })
 
     data.save(filename, df, test=True)
 
@@ -24,10 +32,126 @@ def create_test_data():
 
 
 # testa abrir um arquivo
-def test_data_open(create_test_data):
-    resp = Data().open('for_tests.xlsx')
+def test_data_open(create_test_data, data):
+    resp = data.open('for_tests.xlsx')
 
     assert resp == {'result': 'Success'}
+
+
+# tenta passar valores que gerem erros em open()
+def test_data_open_errors(data):
+    # declara filename com um valor diferente de str
+    filename = data.open(0)
+    assert filename == {'err': 'type_error', 'data': 0, 'expected': 'str'}
+
+    # declara dtype com um valor diferente de str
+    dtype = data.open('filename', dtype=0)
+    assert dtype == {'err': 'type_error', 'data': 0, 'expected': 'str'}
+
+    # declara max_lines com um valor diferente de int
+    max_lines = data.open('filename', max_lines='0')
+    assert max_lines == {'err': 'type_error', 'data': '0', 'expected': 'int'}
+
+    # passa um arquivo que não existe
+    resp = data.open('invalid_data.xlsx')
+    assert resp == {'err': 'data_not_found', 'data': 'spreadsheet', 'filename': 'invalid_data.xlsx'}
+
+    # passa um valor para dtype que não tem suporte
+    dtype_unsupported = data.open('filename', dtype='other')
+    assert dtype_unsupported == {'err': 'type_not_supported', 'type': 'other'}
+
+
+# obtém os valores de uma coluna
+def test_data_get_column(create_test_data, data):
+    data.open('for_tests.xlsx')
+
+    column = data.get_column('name', True).get('data')
+
+    assert column == ['test01', 'test02', 'test03']
+
+
+# tenta passar valores que gerem erros em get_column()
+def test_data_get_column_errors(data):
+    # declara column_name com um valor diferente de str
+    column_name = data.get_column(0)
+    assert column_name == {'err': 'type_error', 'data': 0, 'expected': 'str'}
+
+    # declara to_list com um valor diferente de bool
+    to_list = data.get_column('column_name', to_list=0)
+    assert to_list == {'err': 'type_error', 'data': 0, 'expected': 'bool'}
+
+    # tenta obter uma coluna sem abrir a planilha antes
+    data_undefined = data.get_column('column_name')
+    assert data_undefined == {'err': 'data_not_defined', 'data': 'spreadsheet', 'expected': 'DataFrame'}
+
+
+# obtém os valores várias colunas
+def test_data_get_columns(create_test_data, data):
+    data.open('for_tests.xlsx')
+
+    column = data.get_columns('name', 'years old', to_list=True).get('data')
+
+    assert column == [['test01', 0], ['test02', 0], ['test03', 0]]
+
+
+# tenta passar valores que gerem erros em get_columns()
+def test_data_get_columns_errors(data):
+    # declara column_name com um valor diferente de str
+    column_name = data.get_columns('column_name', 0)
+    assert column_name == {'err': 'type_error', 'data': 0, 'expected': 'str'}
+
+    # declara to_list com um valor diferente de bool
+    to_list = data.get_columns('column_name', to_list=0)
+    assert to_list == {'err': 'type_error', 'data': 0, 'expected': 'bool'}
+
+    # tenta obter uma coluna sem abrir a planilha antes
+    data_undefined = data.get_columns('column_name')
+    assert data_undefined == {'err': 'data_not_defined', 'data': 'spreadsheet', 'expected': 'DataFrame'}
+
+
+# passa valores que gerem erros em new():
+def test_data_new_errors(data):
+    # declara data com um valor diferente de dict
+    dt = data.new(0)
+    assert dt == {'err': 'type_error', 'data': 0, 'expected': 'dict'}
+
+    # passa um dicionário vazio
+    empty = data.new({})
+    assert empty == {'err': 'data_empty',  'data': 'spreadsheet'}
+
+
+# passa valores que gerem erros em save():
+def test_data_save_errors(create_test_data, data):
+    # declara filename com um valor diferente de str
+    filename = data.save(0, {'data': DataFrame()})
+    assert filename == {'err': 'type_error', 'data': 0, 'expected': 'str'}
+
+    # declara data com um tipo de dado diferente de DataFrame ou dicionário sem {'data': 'DataFrame'}
+    df = data.save('filename', data=[])
+    assert df == {'err': 'type_error', 'data': [], 'expected': 'DataFrame'}
+    
+    # passa um dicionário sem {'data': 'DataFrame'}
+    dt = data.save('filename', data={})
+    assert dt == {'err': 'type_error', 'data': {}, 'expected': 'DataFrame'} 
+
+    # declara index com um valor diferente de bool
+    index = data.save('filename', DataFrame(), index=0)  
+    assert index == {'err': 'type_error', 'data': 0, 'expected': 'bool'}
+
+    # declara overwrite com um valor diferente de bool
+    overwrite = data.save('filename', DataFrame(), overwrite=0)  
+    assert overwrite == {'err': 'type_error', 'data': 0, 'expected': 'bool'}
+
+    # tenta criar uma planilha com um nome que já existe
+    exists_filename = data.save('for_tests.xlsx', DataFrame(), overwrite=False)
+    assert exists_filename == {'err': 'spreadsheet_already_exists', 'filename': 'for_tests.xlsx'}
+
+    # tenta salvar uma planiha como for_tests.xlsx sem permitir reescrever o arquivo existente
+    invalid_filename = data.save('for_tests.xlsx', DataFrame())
+    assert invalid_filename == {'err': 'invalid_filename', 'filename': 'for_tests.xlsx'}
+
+
+# o funcionamento correto de save() assim como os seus erros provam que __file_exists() está funcionando
 
 
 if __name__ == '__main__':
